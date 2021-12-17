1bec32d22d6480d4172bf91f5ad5e3eaf1edb96b
AlanOBrien777 <47002646+AlanOBrien777@users.noreply.github.com>
2021-12-17 19:42:48 +0000

    Add files via upload
    updated play activity to change questions to selected quiz

--- app/src/main/java/org/wit/quiz2/activities/PlayActivity.kt
+++ app/src/main/java/org/wit/quiz2/activities/PlayActivity.kt
@@ -2,10 +2,27 @@ package org.wit.quiz2.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.wit.quiz2.R
+import org.wit.quiz2.databinding.ActivityPlayBinding
 
 class PlayActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPlayBinding
+
+    var theQuiz = SplashScreenActivity().playQuiz
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_play)
+        binding = ActivityPlayBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.quizTitle.text = theQuiz.title
+        binding.genre.text = theQuiz.genre
+        binding.q1.text = theQuiz.question1
+        binding.q2.text = theQuiz.question2
+        binding.q3.text = theQuiz.question3
+        binding.q4.text = theQuiz.question4
+        binding.q5.text = theQuiz.question5
+
     }
 }
