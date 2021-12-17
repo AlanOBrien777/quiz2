4e8af0c07ec8a2c30ce6b778fa6041000cae6ba0
AlanOBrien777 <47002646+AlanOBrien777@users.noreply.github.com>
2021-12-17 21:38:28 +0000

    Tried to implement a random quiz selection for playing

--- app/src/main/java/org/wit/quiz2/activities/SplashScreenActivity.kt
+++ app/src/main/java/org/wit/quiz2/activities/SplashScreenActivity.kt
@@ -4,14 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.result.ActivityResultLauncher
+import org.wit.quiz2.databinding.ActivityPlayBinding
 import org.wit.quiz2.databinding.ActivitySplashScreenBinding
+import org.wit.quiz2.models.QuizJSONStore
+import org.wit.quiz2.models.QuizModel
 import timber.log.Timber
 import timber.log.Timber.i
+import java.util.*
 
 class SplashScreenActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashScreenBinding
     private lateinit var refreshIntentLauncher : ActivityResultLauncher<Intent>
+    private lateinit var bind: ActivityPlayBinding
+
+    private var quizzes = mutableListOf<QuizModel>()
+    var playQuiz = QuizModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,8 +35,24 @@ class SplashScreenActivity : AppCompatActivity() {
 
         binding.btnPlay.setOnClickListener() {
             i("play Button Pressed")
-            val intent = Intent(this, QuizListActivity::class.java)
+            val intent = Intent(this, PlayActivity::class.java)
             startActivity(intent)
+
+            randomQuiz()
+
+            bind.quizTitle.text = playQuiz.title
+            bind.genre.text = playQuiz.genre
+            bind.q1.text = playQuiz.question1
+            bind.q2.text = playQuiz.question2
+            bind.q3.text = playQuiz.question3
+            bind.q4.text = playQuiz.question4
+            bind.q5.text = playQuiz.question5
         }
+    }
+
+    fun randomQuiz(){
+        var newId = Random().nextInt(quizzes.size)
+        playQuiz = quizzes[newId]
+        println("random number = $newId")
     }
 }
